9b1ff65fa522e299559398452d4326cb2524380e
bengold1999 <bengold1999@gmail.com>
2024-08-20 14:20:20 +0300

    Add auth and middleware

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,15 @@
+import db from "@/db/db";
+import { PageHeader } from "../../../_comps/pageHeader";
+import { ProductForm } from "../../_comps/ProductForm";
+
+export default async function EditProductPage({ params: { id } }: { params: { id: string } }) {
+
+    const product = await db.product.findUnique({ where: { id } })
+
+    return (
+        <>
+            <PageHeader>Edit Product</PageHeader>
+            <ProductForm product={product}/>
+        </>
+    )
+} 
