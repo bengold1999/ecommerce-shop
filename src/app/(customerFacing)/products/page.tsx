bae4d99346b2ba0c06d613dd3c05ff75e223b6e0
bengold1999 <bengold1999@gmail.com>
2024-08-21 15:04:41 +0300

    Add cache

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,34 +1,37 @@
 import { ProductCard, ProductCardSkeleton } from '@/components/ProductCard'
 import db from '@/db/db'
-import React, { Suspense } from 'react'
+import { cache } from "@/lib/cache"
+
+import React, {  Suspense } from 'react'
 
 const page = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <Suspense
-      fallback={
-        <>
-          <ProductCardSkeleton />
-          <ProductCardSkeleton />
-          <ProductCardSkeleton />
-        </>
-      }
-    >
-      <ProductsSuspense />
-    </Suspense>
-  </div>
-  )
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <Suspense
+                fallback={
+                    <>
+                        <ProductCardSkeleton />
+                        <ProductCardSkeleton />
+                        <ProductCardSkeleton />
+                    </>
+                }
+            >
+                <ProductsSuspense />
+            </Suspense>
+        </div>
+    )
 }
 
 export default page
 
-function getProducts(){
-    return db.product.findMany({
-        where: { isAvailableForPurchase: true },
-        orderBy: { name: "asc" },})
-}
+const getProducts = cache(() => {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { name: "asc" },
+  })
+}, ["/products", "getProducts"])
 
 async function ProductsSuspense() {
-  const products = await getProducts()
-  return products.map((product) => <ProductCard key={product.id} {...product} />)
+    const products = await getProducts()
+    return products.map((product) => <ProductCard key={product.id} {...product} />)
 }
